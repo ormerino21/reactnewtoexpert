be8799f1a599486ff0f2b469b9051a2b7734b94b
Oscar Merino <ormerino21@gmail.com>
2025-07-14 14:25:09 -0600

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import { Button } from './components'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("Jhon")
+
   const countMore = () => {
     setCount((count) => count + 1)
+  }
+
+  const changeName = () => {
+    setName("Jhon Doe")
   }
 
   return (
     <>
       <Button label={`Count is: ${count}`} parentMethod={countMore}/>
+      {name}
+      <Button label={`Change Name`} parentMethod={changeName}/>
     </>
   )
 }
